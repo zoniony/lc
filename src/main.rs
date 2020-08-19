29de30b58f670e9f81480d94c1f64058f6b0da9f
zoniony <1173900767@qq.com>
2020-08-19 11:00:30 +0800

    palindromic_substrings

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+    let a = Solution::count_substrings("abc".to_string());
+    println!("{}", a);
+}
+
+pub struct Solution;
+
+impl Solution {
+    pub fn count_substrings(s: String) -> i32 {
+        let mut counter: i32 = 0;
+        let  s = s.into_bytes();
+        let mut dp = vec![vec![false; s.len()]; s.len()];
+        for j in 0..s.len() {
+            for i in 0..j+1 {
+                if i == j{
+                    dp[i][j] = true;
+                    counter += 1;
+                }
+
+                else if j - i == 1  && s[i] == s[j] {
+                    dp[i][j] = true;
+                    counter += 1;
+                }
+
+                else if j - i > 1 && s[i] == s[j] && dp[i+1][j-1] == true  {
+                    dp[i][j] = true;
+                    counter += 1;
+                } 
+            }
+        }
+        return counter;
+    }
+}
