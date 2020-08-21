b3a0ee6b77fe8efa98a83a97e47288040880a9ea
zoniony <1173900767@qq.com>
2020-08-21 10:26:07 +0800

    unique_paths_ii

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod palindromic_substrings;
 pub mod minimum_path_sum;
+pub mod unique_paths_ii;
--- src/unique_paths_ii.rs
+++ src/unique_paths_ii.rs
@@ -1,23 +1,16 @@
-fn main() {
-    let a = vec![vec![0,0,0],vec![0,1,0],vec![0,0,0]];
-    let b = Solution::unique_paths_with_obstacles(a);
-    println!("{}",b)
-}
-
 struct Solution;
 
 impl Solution {
     pub fn unique_paths_with_obstacles(obstacle_grid: Vec<Vec<i32>>) -> i32 {
         let (m, n) = (obstacle_grid.len(), obstacle_grid[0].len());
         let mut dp = vec![vec![0; n]; m];
-        dp[0][0] = 1;
         for i in 0..m {
             for j in 0..n {
-                if i == 0 && j == 0 {
-                    continue;
-                }
-                else if obstacle_grid[i][j] == 1 {
+                if obstacle_grid[i][j] == 1 {
                     dp[i][j] = 0;
+                }
+                else if i == 0 && j == 0 {
+                    dp[0][0] = 1;
                 }
                 else if i == 0 {
                     dp[i][j] = dp[i][j-1]; 
